247e2de3f4f886e9e81be4230d72076493ae73d6
David Joy <davjoy@gmail.com>
2024-04-11 15:16:43 -0400

    feat: adding the guest iframe to the Host Page.

--- src/host-page/HostPage.jsx
+++ src/host-page/HostPage.jsx
@@ -5,6 +5,7 @@ const ExamplePage = () => (
     <Container className="pt-5">
       <h1>Host Page</h1>
       <p>I am the host page. I share react, react-dom, and @openedx/paragon with my guests.</p>
+      <iframe title="guest" width="100%" height="400" src="http://localhost:8081" />
     </Container>
   </main>
 );
