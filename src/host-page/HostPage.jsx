30a1ffb8e547a31cfa432739de53e05b318bf11e
David Joy <davjoy@gmail.com>
2024-04-11 14:07:27 -0400

    build: making this a traditional MFE

--- src/host-page/HostPage.jsx
+++ src/host-page/HostPage.jsx
@@ -1,29 +1,12 @@
 import { Container } from '@openedx/paragon';
-import {
-  Suspense,
-} from 'react';
-import useFederatedComponent from './data/utils';
 
-const ExamplePage = () => {
-  const url = 'http://localhost:8081/remoteEntry.js';
-  const scope = 'guest';
-  const module = './GuestPage';
-
-  const { Component: FederatedComponent, errorLoading } = useFederatedComponent(url, scope, module);
-
-  return (
-    <main>
-      <Container className="pt-5">
-        <h1>Host Page</h1>
-        <p>I am the host page. I share react, react-dom, and @openedx/paragon with my guests.</p>
-      </Container>
-      <Suspense fallback="Loading...">
-        {errorLoading
-          ? `Error loading module "${module}"`
-          : FederatedComponent && <FederatedComponent />}
-      </Suspense>
-    </main>
-  );
-};
+const ExamplePage = () => (
+  <main>
+    <Container className="pt-5">
+      <h1>Host Page</h1>
+      <p>I am the host page. I share react, react-dom, and @openedx/paragon with my guests.</p>
+    </Container>
+  </main>
+);
 
 export default ExamplePage;
--- src/index.jsx
+++ src/index.jsx
@@ -1,1 +1,37 @@
-import('./bootstrap');
+import 'core-js/stable';
+import 'regenerator-runtime';
+
+import {
+  APP_INIT_ERROR, APP_READY, subscribe, initialize,
+} from '@edx/frontend-platform';
+import { AppProvider, ErrorPage } from '@edx/frontend-platform/react';
+import ReactDOM from 'react-dom';
+
+import Header from '@edx/frontend-component-header/dist/Header';
+import Footer from '@edx/frontend-component-footer/dist/components/Footer';
+import messages from './i18n';
+import HostPage from './host-page/HostPage';
+
+import './index.scss';
+
+subscribe(APP_READY, () => {
+  ReactDOM.render(
+    <AppProvider>
+      <Header />
+      <HostPage />
+      <Footer />
+    </AppProvider>,
+    document.getElementById('root'),
+  );
+});
+
+subscribe(APP_INIT_ERROR, (error) => {
+  ReactDOM.render(<ErrorPage message={error.message} />, document.getElementById('root'));
+});
+
+initialize({
+  messages,
+  handlers: {
+    auth: () => {}, // This MFE turns off auth so it can run independently of edx-platform.
+  },
+});
